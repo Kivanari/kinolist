ec6e41dc5ab93bb132d60f7ccaf917ab656d6598
Kivanari <kiriklrukit@yandex.ru>
2024-06-10 16:46:07 +0300

    2

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,16 +1,1 @@
-const express = require('express');
-const router = express.Router();
 
-router.post('/registration', (req, res) => {
-    res.json({ message: 'Registration successful' });
-});
-
-router.get('/login', (req, res) => {
-    res.json({ message: 'Login page' });
-});
-
-router.get('/auth', (req, res) => {
-    res.json({ message: 'ura' });
-});
-
-module.exports = router;
